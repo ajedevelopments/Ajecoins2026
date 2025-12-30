3c5f3656ebe9d7eb0608c00cebda8b52838883c8
marcosalmeidaec-star <marcos.almeida.ec@ajegroup.com>
2025-12-30 12:30:04 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsz2EP8IsTlG02uU2_GRfyQeeajMDuJjI",
+  authDomain: "ajecoins-73829.firebaseapp.com",
+  projectId: "ajecoins-73829",
+  storageBucket: "ajecoins-73829.firebasestorage.app",
+  messagingSenderId: "247461322350",
+  appId: "1:247461322350:web:802185ad39249ca650507f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// hacemos db global
+window.db = db;
+
+console.log("🔥 Firebase conectado correctamente");
